099289cbb40311187a66c9979050b870aeb66e71
krisrajz <krisrajz@gmail.com>
2022-09-11 15:00:12 +0530

    Tied up web service with form

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,31 @@
-console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const weatherSection = document.querySelector('#weather')
+const alertSection = document.querySelector('#message')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        if(data.error)
-            console.log(error)
-        else
-            console.log(data)
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault() //prevents form refresh
+    const location = search.value
+
+    const weatherapiUrl = 'http://localhost:3000/weather?address=' + location
+
+    alertSection.textContent = 'Fetching weather data ...'
+
+    fetch(weatherapiUrl).then((response) => {
+        response.json().then((data) => {
+            if(data.error)
+                alertSection.textContent = data.error
+            else
+            {
+                alertSection.textContent = ''
+                weatherSection.innerHTML = 
+                'Location: ' + data.locationDetails.location +
+                '<br>Latitude: ' + data.locationDetails.latitude +
+                '<br>Longitude: ' + data.locationDetails.longitude +
+                '<br>Weather: ' + data.weatherDetails.weather[0] + 
+                '<br>Temperature: ' + data.weatherDetails.temperature +
+                '<br>Feels like: ' + data.weatherDetails.feelslike
+            }
     })
 })
-
-fetch('http://localhost:3000/weather?address=Chennai').then((response) => {
-    response.json().then((data) => {
-        if(data.error)
-            console.log(data.error)
-        else
-            console.log(data)
-    })
 })
